031e1ee510a67c16048a844fc9c3984f2bd88ffd
hunterxcobby <solomonsefah13@gmail.com>
2024-01-09 15:45:20 +0000

    js is weakly typed

---
+++ basics/data_structure/practice/example.js
@@ -0,0 +1,7 @@
+
+const foo = 42; // foo is a number
+const result = foo + "2"; // JavaScript coerces foo to a string, so it can be concatenated with the other operand
+console.log(result); // 421
+
+type = typeof(result)
+console.log(type)
