1dd9ad53226bdc84d81f89d7efe3f7dd8ae8d48f
hunterxcobby <solomonsefah13@gmail.com>
2024-04-02 16:23:00 +0000

    remove old and unused files

---
+++ lessons/basics/conditionals/example.js
@@ -0,0 +1,24 @@
+
+a = 3 + 5
+console.log(a);
+
+a = 3 - 5
+console.log(a);
+
+a = '3' + '5'
+console.log(a);
+
+a = "hello" + "world";
+console.log(a);
+
+a = 3 == 5
+console.log(a);
+
+a = 3 != 5
+console.log(a);
+
+a = 3 !== 5
+console.log(a);
+
+a = !(3 != 5)
+console.log(a);
