87a3b69f1afe2da4831ef4ce9f85c7f9a9e3041d
hunterxcobby <solomonsefah13@gmail.com>
2024-04-05 16:26:12 +0000

    update old notes and files

---
+++ lessons/basics/GENERALS/practice2.js
@@ -0,0 +1,20 @@
+
+// functions declaration
+
+function greeting (name) {
+    console.log(`Greetings ${name}`);
+}
+
+greeting('Cobby');
+
+
+function add (num1, num2) {
+    return num1 + num2;
+}
+
+console.log(add(3, 6));
+
+
+const multiply = (x, y) => x * y;
+
+console.log(multiply(5, 10))
