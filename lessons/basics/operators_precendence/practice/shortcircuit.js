f500b17c55b8d9fb1608c417e0b7fd3898358412
hunterxcobby <solomonsefah13@gmail.com>
2024-04-06 16:27:35 +0000

    update old notes and files

---
+++ lessons/basics/operators_precendence/practice/shortcircuit.js
@@ -0,0 +1,8 @@
+function A() { console.log('called A'); return false; }
+function B() { console.log('called B'); return false; }
+function C() { console.log('called C'); return true; }
+
+console.log(C() || B() && A());
+// Logs:
+// called C
+// true
