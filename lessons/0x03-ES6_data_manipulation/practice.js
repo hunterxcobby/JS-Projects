f36ead5950bc6014915b3e712a1077ac0a944ea0
Cobby Sefah Solomon <solomonsefah13@gmail.com>
2024-04-07 11:55:24 +0000

    the for each method

--- lessons/0x03-ES6_data_manipulation/practice.js
+++ lessons/0x03-ES6_data_manipulation/practice.js
@@ -14,6 +14,11 @@ const companies = [
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 // for loop
-for (i = 0; i < companies.length; i++) {
-    console.log(companies[i]);
-}
+// for (i = 0; i < companies.length; i++) {
+//     console.log(companies[i]);
+// }
+
+// for each 
+companies.forEach(function(company, index) {
+    console.log(company);
+});
