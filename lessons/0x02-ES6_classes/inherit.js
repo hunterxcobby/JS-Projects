e03f66461ce08e174d45b365b9fdde689db1f1ca
Cobby Sefah Solomon <solomonsefah13@gmail.com>
2024-04-08 15:45:58 +0000

    add a chapter on clasees

--- lessons/0x02-ES6_classes/inherit.js
+++ lessons/0x02-ES6_classes/inherit.js
@@ -15,3 +15,17 @@ console.log(dog.made_sound());
 
 const cat = new Animal("Cat", "Meow Meow");
 console.log(cat.made_sound());
+
+
+class Dog extends Animal {
+    constructor(name, sound) {
+        super(name, sound)
+    }
+
+    made_sound() {
+        return `The sound that ${this.name} makes is ${this.sound}`
+    }
+}
+
+const d = new Dog("Dog", "Woof woof")
+console.log(d.made_sound())
