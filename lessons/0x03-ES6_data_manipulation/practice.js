161faa69a98c3b4b871fdfdc6da360bd845c46ce
Cobby Sefah Solomon <solomonsefah13@gmail.com>
2024-04-28 11:50:17 +0000

    practice data manipulation

--- lessons/0x03-ES6_data_manipulation/practice.js
+++ lessons/0x03-ES6_data_manipulation/practice.js
@@ -13,3 +13,7 @@ const companies = [
 
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
+// for loop
+for (i = 0; i < companies.length; i++) {
+    console.log(companies[i]);
+}
