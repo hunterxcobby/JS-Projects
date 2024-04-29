f086e9da8f76c24b7c0f208c8bfa3f64fe10a7d5
Cobby Sefah Solomon <solomonsefah13@gmail.com>
2024-04-29 14:41:30 +0000

    Refactor sort method in practice.js for data manipulation

--- lessons/0x03-ES6_data_manipulation/practice.js
+++ lessons/0x03-ES6_data_manipulation/practice.js
@@ -82,13 +82,15 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 
 // sort 
-const sortedCompanies = companies.sort(function(c1, c2){
-    if(c1.start > c2.start)
-    {
-        return 1;
-    }else {
-        return -1;
-    }
-});
+// const sortedCompanies = companies.sort(function(c1, c2){
+//     if(c1.start > c2.start)
+//     {
+//         return 1;
+//     }else {
+//         return -1;
+//     }
+// });
+
+const sortedCompanies = companies.sort((a, b) => (a.start > b.start ? 1 : -1)); 
 
 console.log(sortedCompanies);
