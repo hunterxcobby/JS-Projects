0e6d1b13bfab1d8b6c1ff201cb72f09c37ee32d0
Cobby Sefah Solomon <solomonsefah13@gmail.com>
2024-04-29 12:50:10 +0000

    Refactor forEach method and add filter and map methods for data manipulation in practice.js

--- lessons/0x03-ES6_data_manipulation/practice.js
+++ lessons/0x03-ES6_data_manipulation/practice.js
@@ -64,4 +64,13 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 // const testMap = companies.map( company => company.name)
 
+const testMap = companies.map(function(company) {
+    if (company.category === "Retail") {
+        return `This company ${company.name} was created at ${company.start}`
+    }
+    else{
+        return "company not found";
+    }
+});
+
 console.log(testMap);
