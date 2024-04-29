9d4867858ee3f075e53422785507125a896bb638
Cobby Sefah Solomon <solomonsefah13@gmail.com>
2024-04-29 12:44:11 +0000

    Refactor forEach method and add filter and map methods for data manipulation in practice.js

--- lessons/0x03-ES6_data_manipulation/practice.js
+++ lessons/0x03-ES6_data_manipulation/practice.js
@@ -52,6 +52,16 @@ const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 //     }
 // });
 
-const companiesRetail = companies.filter(company => company.category === "Retail")
+// const companiesRetail = companies.filter(company => company.category === "Retail")
 
-console.log(companiesRetail);
+// console.log(companiesRetail);
+
+// map
+
+// const testMap = companies.map(function(company) {
+//     return company.name;
+// });
+
+// const testMap = companies.map( company => company.name)
+
+console.log(testMap);
