249cf5bd762740b6506f7d80aa25415ba9e971d7
Cobby Sefah Solomon <solomonsefah13@gmail.com>
2024-05-07 16:58:02 +0000

    Add TypeScript file for logging points

---
+++ lessons/0x04-TypeScript/5-min_typescript/typesystem.ts
@@ -0,0 +1,12 @@
+interface Point {
+    x: number;
+    y: number;
+  }
+  
+  function logPoint(p: Point) {
+    console.log(`${p.x}, ${p.y}`);
+  }
+  
+  const point = { x: 12, y: 26 };
+  logPoint(point); // logs "12, 26"
+  
